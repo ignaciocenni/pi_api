e5a7fc6e11fac12b3e418883ba5761e70826d2f0
ignaciocenni <ignaziocenni@gmail.com>
2023-06-03 04:04:23 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
+/*
 const sequelize = new Sequelize(
 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
 	{
@@ -11,6 +12,12 @@ const sequelize = new Sequelize(
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 	}
 );
+*/
+
+const sequelize = new Sequelize(DB_DEPLOY, {
+	logging: false,
+	native: false,
+});
 
 const basename = path.basename(__filename);
 
